abbd846247c13db1074c0e868995f9e038fd06c9
elledienne <elledienne@gmail.com>
2015-10-07 15:06:46 -0700

    stack queue

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj.length = 0;
+  obj.storage = {};
+  return obj;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  push: function(value){
+    this.length++;
+    this.storage[this.length] = value;
+  },
+  pop: function(){
+    if(this.length > 0){
+      var result = this.storage[this.length];
+      delete this.storage[this.length];
+      this.length--;
+      return result;
+    }
+  },
+  size: function(){
+    return this.length;
+  }
+};
