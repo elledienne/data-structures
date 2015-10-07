d249dd0ce49bd4060de2cdabea65fa68b1e6a5a0
elledienne <elledienne@gmail.com>
2015-10-07 15:36:07 -0700

    linked lists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    //first item in list
+    if (!list.head) {
+      list.head = node;
+    // any subsequent item   
+    } else {
+      list.tail.next = node;
+    }
+    list.tail = node;
   };
 
   list.removeHead = function() {
+    var toReturn = list.head.value;
+    list.head = list.head.next;
+    return toReturn;
   };
 
-  list.contains = function(target) {
+  list.contains = function(target, listItem) {
+    var listItem = listItem || list.head;
+    if (listItem.value === target) {
+      return true;
+    }
+    else if (listItem.next === null) {
+      return false;
+    }
+    else {
+      return (list.contains(target, listItem.next))
+    }
   };
 
   return list;
