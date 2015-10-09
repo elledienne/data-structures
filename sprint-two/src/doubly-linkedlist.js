f7114d99aa7cdf345ac024d0813a35b4200bc41e
elledienne <elledienne@gmail.com>
2015-10-09 16:11:33 -0700

    doubly linked list

---
+++ sprint-two/src/doubly-linkedlist.js
@@ -0,0 +1,80 @@
+var LinkedList = function() {
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+  list.addToTail = function(value) {
+    var node = Node(value);
+    //first item in list
+    if (!list.head) {
+      list.head = node;
+      // any subsequent item   
+    } else {
+      node.previous = list.tail;
+      list.tail.next = node;
+    }
+    list.tail = node;
+  };
+
+  list.removeHead = function() {
+    var toReturn = list.head.value;
+    list.head = list.head.next;
+    list.head.previous = null;
+    return toReturn;
+  };
+
+  // list.contains = function(target, listItem) {
+  //   var listItem = listItem || list.head;
+  //   if (listItem.value === target) {
+  //     return true;
+  //   } else if (listItem.next === null) {
+  //     return false;
+  //   } else {
+  //     return (list.contains(target, listItem.next))
+  //   }
+  // };
+
+  list.contains = function(target, listItem) {
+    var listItem = listItem || list.head;
+    if (listItem.value === target) {
+      return true;
+    } else {
+      while (listItem.next) {
+        listItem = listItem.next;
+        if (listItem.value === target) {
+          return true;
+        }
+      }
+      return false;
+    }
+  };
+
+  return list;
+};
+
+list.addToHead = function(value) {
+  var node = new Node(value);
+  list.head.previous = node;
+  node.next = list.head;
+  list.head = node;
+};
+list.removeTail = function() {
+  var tailToBeRemoved = list.tail;
+  list.tail = list.tail.previous;
+  list.tail.next = null;
+  tailToBeRemoved.previous = null;
+};
+
+var Node = function(value) {
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+  node.previous = null;
+
+  return node;
+};
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ */
