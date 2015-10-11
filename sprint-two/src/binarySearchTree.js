8cb713442000aadb303c705fe909c406e54b3e8b
elledienne <elledienne@gmail.com>
2015-10-10 17:37:18 -0700

    implemented collapse tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,10 +1,13 @@
 var BinarySearchTree = function(value) {
 	var root = Node(value);
 	var binarySearchTree = root
+	var depthLeft = 1;
+	var depthRight = 1; 
 
 	binarySearchTree.insert = function(value, parent) {
 		var parent = parent || root;
 		var node = Node(value);
+		node.depth = parent.depth + 1;
 
 		if (parent.value > value) {
 			if (!parent.left) {
@@ -12,13 +15,23 @@ var BinarySearchTree = function(value) {
 			} else {
 				this.insert(value, parent.left);
 			}
+			if(node.depth > depthLeft) {
+				depthLeft++;
+			}
 		} else if (parent.value < value) {
 			if (!parent.right) {
 				parent.right = node;
 			} else {
 				this.insert(value, parent.right)
 			}
+			if(node.depth > depthRight) {
+				depthRight++;
+			}
 		}
+		// if(depthLeft/depthRight === 2 || depthLeft/depthRight === .5) {
+		// 	this.rebalanceTree();
+		// }
+
 	};
 
 	binarySearchTree.contains = function(target, item) {
@@ -48,6 +61,18 @@ var BinarySearchTree = function(value) {
 		}
 	};
 
+	binarySearchTree.collapseTree = function(node) {
+		node = node || root;
+		var result = [node.value];
+		if (node.left) {
+			result = this.collapseTree(node.left).concat(result);
+		}
+		if (node.right) {
+			result = result.concat(this.collapseTree(node.right));
+		}
+		return result;
+	}
+
 	return binarySearchTree;
 }
 
@@ -57,9 +82,17 @@ var Node = function(value) {
 		node.value = value;
 		node.left = null;
 		node.right = null;
+		node.depth = 1;
 
 		return node;
 	}
+
+binarySearchTree = BinarySearchTree(5);
+binarySearchTree.insert(2);
+binarySearchTree.insert(3);
+binarySearchTree.insert(7);
+binarySearchTree.insert(6);
+console.log(binarySearchTree.collapseTree());
 	/*
 	 * Complexity: What is the time complexity of the above functions?
 	 */
